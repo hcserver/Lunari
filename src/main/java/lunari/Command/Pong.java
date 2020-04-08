48b179a17ff25171200bbb3bbb91506ca1f36607
hcserver <63043013+hcserver@users.noreply.github.com>
2020-04-08 16:19:16 +0100

    Updated Pong Command

--- src/main/java/lunari/Command/Pong.java
+++ src/main/java/lunari/Command/Pong.java
@@ -4,7 +4,8 @@ import org.javacord.api.event.message.MessageCreateEvent;
 
 public class Pong {
     public Pong(MessageCreateEvent event) {
-        if (event.getMessageContent().equalsIgnoreCase("!pong")) {
+        String MessageContent = event.getMessageContent().toLowerCase();
+        if(IsCommand.Check(MessageContent, "pong")){
             event.getChannel().sendMessage("Ping!");
         }
     }
